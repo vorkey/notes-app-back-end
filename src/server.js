14d266e806482b88709e1e1066509204916023ae
Ambadaya Gaman <ambadayaakun@gmail.com>
2024-04-02 11:06:48 +0700

    feat: cors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.Server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.routes = routes;
